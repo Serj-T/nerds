e408e8aa14535854f77f47c31f04ea9faf1dc4af
Serj T <serjtelepov@gmail.com>
2017-02-21 19:48:31 +0300

    +build

--- gulpfile.js
+++ gulpfile.js
@@ -6,11 +6,16 @@ var	plumber = require('gulp-plumber'); /*продолжает работу watch
 var postcss = require('gulp-postcss'); /*для autoprefixer*/
 var prefix = require('autoprefixer'); /*плагин для postcss*/
 var	server = require('browser-sync').create(); /*обновление localhost*/
+var rename = require("gulp-rename");
+var minify = require("gulp-csso");
+var imagemin = require("gulp-imagemin");
+var run = require("run-sequence");
+var clean = require("del");
 
 gulp.task('style', function() {
 	gulp.src('less/style.less') /*берем файл для обработки*/
 		.pipe(plumber())
-		.pipe(less()) 
+		.pipe(less())
 		.pipe(postcss( /*указываем плагин postcss*/
 			[prefix({browser:[ /*указываем поддерживаемые браузеры*/
 				"last 1 version",
@@ -20,17 +25,49 @@ gulp.task('style', function() {
  				"last 2 Edge versions" ]})
 			]))
 		.pipe(gulp.dest('css')) /*складываем файл после обработки в папку*/
+		.pipe(minify())
+		.pipe(rename("style.min.css"))
+    .pipe(gulp.dest("css"))
+    .pipe(gulp.dest("build/css"))
 		.pipe(server.stream());
 });
 
 gulp.task('sync', ['style'], function() {
 	server.init({
-		server: ".", /*стартует из корня*/
+		server: "build", /*стартует из корня*/
 		notify: false, /*убираем оповещения*/
     open: true,
     ui: false
-	});
+});
 
 	gulp.watch("less/**/*.less", ['style']);
 	gulp.watch("*.html").on("change", server.reload); /*следим за обновлениями html*/
+});
+
+gulp.task("imagemin", function() {
+  return gulp.src("img/**/*.{jpg,png}")
+    .pipe(imagemin([
+      imagemin.jpegtran({progressive: true})
+    ]))
+    .pipe(gulp.dest("build/img"));
+});
+
+gulp.task("copy", function() {
+  return gulp.src([
+    "fonts/**/*.{woff,woff2}",
+    "img/**/*",
+    "js/**",
+    "*.html"
+  ], {
+  base: "."
+  })
+  .pipe(gulp.dest("build"));
+});
+
+gulp.task("clean", function() {
+  return clean("build");
+});
+
+gulp.task("build", function(fn) {
+  run("clean", "copy", "style", "imagemin", fn);
 });
